88338d622e9df337ecac1fe24cb77dfe3dc84f14
HuynhMinhLuan-B2106842 <155254424+HuynhMinhLuan-B2106842@users.noreply.github.com>
2025-06-02 19:09:21 +0700

    Delete backend_dynamic_process directory

--- backend_dynamic_process/src/routes/Connection.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/Connection.controller');
-
-router.get('/', controller.getConnections);
-router.post('/', controller.createConnection);
-router.delete('/:id', controller.deleteConnection);
-
-module.exports = router;
