e51bc0a086f1771b9f3824a1f2d97b034dbc0266
HuynhMinhLuan-B2106842 <155254424+HuynhMinhLuan-B2106842@users.noreply.github.com>
2025-06-18 16:00:21 +0700

    quytrinhtungbuoc

---
+++ backend/models/QuyTrinh.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Buoc = require('./Buoc');
+
+const quyTrinhSchema = new mongoose.Schema({
+  ten: {
+    type: String,
+    required: true,
+  },
+  maBenhNhan: {
+    type: String,
+    required: true,
+  },
+  tenBenhNhan: {
+    type: String,
+    required: true,
+  },
+  ngayBatDau: {
+    type: Date,
+    default: Date.now,
+  },
+  trangThai: {
+    type: String,
+    enum: ['cho_xu_ly', 'dang_xu_ly', 'hoan_thanh'],
+    default: 'cho_xu_ly',
+  },
+  buocHienTai: {
+    type: String,
+    default: null,
+  },
+  diagramId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Diagram',
+    required: true,
+  },
+  cacBuoc: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Buoc',
+  }],
+  ngayCapNhat: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hàm chạy trước khi lưu để cập nhật ngày
+quyTrinhSchema.pre('save', async function(next) {
+  if (this.isModified('cacBuoc')) {
+    this.ngayCapNhat = Date.now();
+  }
+  next();
+});
+
+module.exports = mongoose.model('QuyTrinh', quyTrinhSchema);
